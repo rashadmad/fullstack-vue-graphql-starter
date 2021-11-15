fa814affb54a85c508680ce4be9c0e3dd6fb627c
rashad madison <rashadmad@rashads-MBP.attlocal.net>
2021-11-15 02:34:43 -0500

    get server up and running

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, gql } = require('apollo-server');
 
-const server = new ApolloServer({});
+const todos = [
+    { task: 'wash car', completed: false },
+    { task: 'clean car', completed: true },
+];
 
-server.listen();
+const typeDefs = gql` 
+    type Query {
+        task: String
+        completed: Boolean
+    }
+`;
+
+const server = new ApolloServer({
+    typeDefs: typeDefs
+});
+
+server.listen(4500).then(({ url }) => {
+    console.log(`server listening`)
+});
